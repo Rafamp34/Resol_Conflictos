f08d080557e88b322040af890628cb086cf13c88
Rafamp34 <rafaelmoncayop37@gmail.com>
2024-04-08 20:29:01 +0200

    first commit

---
+++ Ejer1.java
@@ -0,0 +1,17 @@
+package Tema2;
+
+    public class Ejer1 {
+
+    public static void main(String[] args)  {
+            int x =144, y =999;
+            System.out.println("x = "+ x);
+            System.out.println("y = "+ y);
+            System.out.println("x + y = "+ (x+y));
+            System.out.println("x - y = "+ (x-y));
+            System.out.println("x * y = "+ (x*y));
+            System.out.println("x / y = "+ (x/y));
+
+        }
+
+        //Esta linea esta en main
+    }
